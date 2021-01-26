258f8983a68945126318dccdb3e07c3b7164604a
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-01-26 18:42:54 +0100

    locale edit buttons

---
+++ src/Parameter/EditLocaleParameter.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Pars\Helper\Parameter;
+
+
+class EditLocaleParameter extends AbstractParameter
+{
+    protected static string $name = 'editlocale';
+
+    /**
+     * Parameter constructor.
+     * @param string $value
+     * @throws \Niceshops\Core\Exception\AttributeExistsException
+     * @throws \Niceshops\Core\Exception\AttributeLockException
+     */
+    public function __construct(string $value)
+    {
+        $this->setAttribute($value, '');
+    }
+
+    public static function name(): string
+    {
+        return self::$name;
+    }
+
+
+}
