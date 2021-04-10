7a01e55ec18e17d694336737879398a25170c2ae
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-10 17:42:56 +0200

    caching

--- src/Debug/DebugHelper.php
+++ src/Debug/DebugHelper.php
@@ -18,23 +18,10 @@ class DebugHelper
      * @param $obj
      * @param int $level
      */
-    public static function trace($obj, int $level = 10)
+    public static function trace($obj, int $level = 15)
     {
-        $trace = debug_backtrace(null, $level);
-        $item = $trace[0];
-        while (in_array($item['function'], ['trace', 'pars_debug'])) {
-            array_shift($trace);
-            $item = $trace[0];
-        }
-        $class = $item['class'] ?? null;
-        $debug = '<pre style="font-size: 14px; font-weight: bolder">DEBUG: ' . $class . ':' . $item['function'] . ':' . $item['line'] . '</pre>';
-        $debug .= '<pre style="font-size: 13px">';
-        $debug .= '<br>--------------TRACE--------------<br>';
-        foreach ($trace as $i => $item) {
-            $class = $item['class'] ?? null;
-            $debug .= '<br>#' . $i . ' - ' . $class . ':' . $item['function'] . ':' . $item['line'];
-        }
-        $debug .= '<br><br>--------------DUMP--------------<br><br>';
+        $debug = self::getBacktrace($level);
+        $debug .= '<pre><br><br>--------------DUMP--------------<br><br>';
         if (is_scalar($obj)) {
             $dump = var_export($obj, true);
         } else {
@@ -47,6 +34,26 @@ class DebugHelper
         $debug .= $dump;
         $debug .= '</pre>';
         self::$debug .= $debug . '<br>';
+    }
+
+    public static function getBacktrace(int $level = 15, array $ignoreFunctions = []): string
+    {
+        $trace = debug_backtrace(null, $level);
+        $item = $trace[0];
+        while (in_array($item['function'], array_merge(['trace', 'pars_debug', 'getBacktrace'], $ignoreFunctions))) {
+            array_shift($trace);
+            $item = $trace[0];
+        }
+        $class = $item['class'] ?? null;
+        $debug = '<pre style="font-size: 14px; font-weight: bolder">DEBUG: ' . $class . ':' . $item['function'] ?? '' . ':' . $item['line'] ?? '' . '</pre>';
+        $debug .= '<pre style="font-size: 13px">';
+        $debug .= '<br>--------------TRACE--------------<br>';
+        foreach ($trace as $i => $item) {
+            $class = $item['class'] ?? null;
+            $debug .= '<br>#' . $i . ' - ' . $class . ':' . $item['function'] ?? '' . ':' . $item['line'] ?? '';
+        }
+        $debug .= '</pre>';
+        return $debug;
     }
 
     /**
