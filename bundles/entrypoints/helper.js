8666fa0ce749c7651f44eaaff2a3f0ac80e1788a
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-04-18 23:11:52 +0200

    cleanup js

--- bundles/entrypoints/helper.js
+++
@@ -1,6 +0,0 @@
-import {ServerEventHelper} from "../js/ServerEventHelper/EventHelper";
-window.eventHelper = new ServerEventHelper(document);
-
-document.addEventListener("DOMContentLoaded", () => {
-    window.eventHelper.init();
-});
