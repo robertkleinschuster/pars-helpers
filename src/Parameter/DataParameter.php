38c62a40fd6b4e50c9312b2236cde4c3708b8a99
robertkleinschuster <robert.kleinschuster@niceshops.com>
2021-05-21 17:47:23 +0200

    feat(Admin): Dynamic form fields

--- src/Parameter/DataParameter.php
+++ src/Parameter/DataParameter.php
@@ -6,9 +6,17 @@ namespace Pars\Helper\Parameter;
 
 class DataParameter extends AbstractParameter
 {
+
     public static function name(): string
     {
         return 'data';
     }
 
+    public function fromArray(array $parameter): AbstractParameter
+    {
+        foreach ($parameter as $key => $value) {
+            $this->setAttribute($key, urldecode($value));
+        }
+        return $this;
+    }
 }
